a3ff35d352587df048da313f86f0fc35d712cf09
Rohan Mehta <1411081+rohan-git@users.noreply.github.com>
2017-11-21 17:19:21 -0500

    Initial commit 2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send({hi: 'there'});
+  res.send({hi: 'hello there! sample application 1st change!'});
 });
 
 const PORT = process.env.PORT || 5000;
