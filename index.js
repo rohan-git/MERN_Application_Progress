c12644f720b5cb99a9fb31789157f47916094062
Rohan Mehta <1411081+rohan-git@users.noreply.github.com>
2017-11-22 10:41:38 -0500

    s

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({hi: 'hello there! sample application 1st change!'});
+passport.use(
+    new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    accessToken =>
+    {
+          console.log('accessToken:', accessToken);
+    })
+);
+
+app.get('/auth/google', passport.authenticate('google',
+{
+    scope: ['profile', 'email']
 });
 
 const PORT = process.env.PORT || 5000;
