a3acbe77f4613e3a64ca4d579e71001c103407b2
Rohan Mehta <1411081+rohan-git@users.noreply.github.com>
2017-11-22 18:13:35 -0500

    use cookie session; spelling -- `keys: [ , , ]`

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+
 const keys = require('./config/keys');
 
 require('./models/User');
 require('./services/passport');
 
-console.log('keys', keys.mongoURI);
+console.log('mongo uri key --- ', keys.mongoURI);
 mongoose.connect(keys.mongoURI);
 
+console.log('cookie key --- ', [keys.cookieKey]);
+
 const app = express();
+
+app.use(cookieSession({
+  maxAge: 30 * 24 * 60 * 60 * 1000,
+  keys: [keys.cookieKey]
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
