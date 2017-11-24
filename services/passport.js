573fd31012c82135be82947b5ec9a24d5dee9056
rohan <1411081+rohan-git@users.noreply.github.com>
2017-11-24 17:27:20 -0500

    added settings to redirect from proxies

--- services/passport.js
+++ services/passport.js
@@ -25,7 +25,8 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback'
+      callbackURL: '/auth/google/callback',
+      proxy: true
     },
     (accessToken, refreshToken, profile, done) =>
     {
