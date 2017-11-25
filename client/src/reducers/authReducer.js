0109b3bf90259bcc213cf2239ac440ec7edbb5b4
rohan <1411081+rohan-git@users.noreply.github.com>
2017-11-25 14:09:02 -0500

    added reducer comp; authReducer to begin handling login on frontend

---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const MY_CONST = 'MY_CONST';
+
+export default function (state = {}, action) {
+  switch (action.type) {
+    case MY_CONST:
+
+      break;
+    default:
+      return state;
+
+  }
+}
