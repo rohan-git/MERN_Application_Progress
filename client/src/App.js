a4bb54329da5d751fc1a1fa33c2133cb00110828
rohan <1411081+rohan-git@users.noreply.github.com>
2017-11-24 19:07:57 -0500

    relative link won't work as server and client are on diff ports

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ class App extends Component {
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+          <a href="/auth/google"> Login using google </a>
         </p>
       </div>
     );
