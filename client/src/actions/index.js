1c14ce8536054a9902acab8562b3ca14e5b6b57c
rohan <1411081+rohan-git@users.noreply.github.com>
2017-11-26 11:33:40 -0500

    extract `res.data` in action

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -6,10 +6,8 @@ export const fetchUser = () => async (dispatch) => {
 
   const res = await axios.get('api/current_user');
 
-    console.log('FETCH_USER', FETCH_USER);
+  console.log('FETCH_USER', FETCH_USER);
+  dispatch({ type: FETCH_USER, payload: res.data });
 
-    dispatch({ type: FETCH_USER, payload: res });
-
-  });
 
 }
