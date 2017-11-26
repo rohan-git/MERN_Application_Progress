5267e985795419f3b7a69ac8975979fa7a9476bf
rohan <1411081+rohan-git@users.noreply.github.com>
2017-11-26 11:10:46 -0500

    formatting

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,16 +1,17 @@
 import axios from 'axios';
 
-import { FETCH_USER } from './action_types';
+import {FETCH_USER} from './action_types';
 
 export const fetchUser = () => {
 
   return function(dispatch){
 
-    axios.get('api/current_user').then(res =>
+    axios.get('api/current_user').then(res => {
 
-      dispatch({ type: FETCH_USER, payload: res })
+      console.log('FETCH_USER', FETCH_USER);
 
-    )
+      dispatch({ type: FETCH_USER, payload: res });
+
+    });
   }
-
 }
--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -4,8 +4,9 @@ export const MY_CONST = 'MY_CONST';
 
 export default function (state = {}, action) {
 switch (action.type) {
-    case MY_CONST:
-
+    case FETCH_USER:
+        console.log('FETCH_USER reducer action:', action);
+        return action;
       break;
     default:
       return state;
