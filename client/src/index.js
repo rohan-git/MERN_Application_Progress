581e26263571075e1eea7f48b67a0425d84a9028
rohan <1411081+rohan-git@users.noreply.github.com>
2017-11-25 19:12:41 -0500

    added redux thunk package

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
 
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
@@ -9,7 +10,7 @@ import registerServiceWorker from './registerServiceWorker';
 import App from './components/App';
 import reducers from './reducers/index.js';
 
-const store = createStore(reducers, {}, applyMiddleware())
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk))
 
 ReactDOM.render(
   <Provider store={store}><App /></Provider>, document.getElementById('root'));
