4c507e2d22ca7eff1f3f9e3d5d9e41a1f6027d59
Rohan Mehta <1411081+rohan-git@users.noreply.github.com>
2017-11-27 15:43:03 -0500

    added redirect

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,12 +3,18 @@ const passport = require('passport');
 module.exports = app => {
 
   app.get('/auth/google/', passport.authenticate('google', {
+
       scope: ['profile', 'email']
   }));
 
-  app.get('/auth/google/callback', passport.authenticate('google'));
+  app.get('/auth/google/callback', passport.authenticate('google'), (req, res) => {
+
+    //console.log('-- > redicrecting to suervyes: ', res);
+    res.redirect('/surveys/');
+  });
 
   app.get('/api/logout', (req, res) => {
+
     req.logout();
     res.send(req.user);
   });
