0aef42dd9aead10b59d656b47c9b4361b8b09904
Rohan Mehta <1411081+rohan-git@users.noreply.github.com>
2017-11-28 14:28:33 -0500

    moved authentication checker to its own middleWare

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,21 +1,16 @@
 const keys = require('../config/keys');
 const stripe = require('stripe')( keys.stripeSecretKey );
+const requireLogin = require('../middlewares/requireLogin');
 
 module.exports = app => {
 
-  app.post('/api/stripe/', async (req, res) => {
+  app.post('/api/stripe', requireLogin, async (req, res) => {
 
       console.log('-- > req.body ', req.body);
 
-      if(!req.user){
-        return res.status(401).send({
-          error: "You must log in !!"
-        });
-      }
-
       const charge = await stripe.charges.create({
         amount: 500,
-        currenct: 'usd',
+        currency: 'usd',
         description: '$5 for 5 credits',
         source: req.body.id
       });
@@ -23,7 +18,7 @@ module.exports = app => {
       req.user.credits += 5;
       const user = await req.user.save();
 
-      console.log(charge);
+      console.log('charge', charge);
     }
   );
 
