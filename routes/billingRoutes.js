1d9911399c56dbadab4f91d2c1a41f91e642478f
Rohan Mehta <1411081+rohan-git@users.noreply.github.com>
2017-11-28 11:52:49 -0500

    backend charge on token;; save the added credits ;; edit user Model to store their credits

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,8 +1,30 @@
+const keys = require('../config/keys');
+const stripe = require('stripe')( keys.stripeSecretKey );
 
 module.exports = app => {
 
-  app.post('/api/stripe/', (req, res) => {
+  app.post('/api/stripe/', async (req, res) => {
 
-  });
+      console.log('-- > req.body ', req.body);
+
+      if(!req.user){
+        return res.status(401).send({
+          error: "You must log in !!"
+        });
+      }
+
+      const charge = await stripe.charges.create({
+        amount: 500,
+        currenct: 'usd',
+        description: '$5 for 5 credits',
+        source: req.body.id
+      });
+
+      req.user.credits += 5;
+      const user = await req.user.save();
+
+      console.log(charge);
+    }
+  );
 
 }
