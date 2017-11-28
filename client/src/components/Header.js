7e4ad9b4f5cf4ee35462ea7d8f58768afd16b3e7
Rohan Mehta <1411081+rohan-git@users.noreply.github.com>
2017-11-28 14:30:01 -0500

    header shows credit amount

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -7,7 +7,7 @@ import Payments from './Payments';
 class Header extends Component {
 
   constructor(props){
-    
+
     super(props);
   }
 
@@ -24,7 +24,8 @@ class Header extends Component {
       default:
         return [
                  <li key="1"><Payments /></li> ,
-                 <li key="2"><a href="/api/logout">Logout</a></li>
+                 <li key="2">Credits: {this.props.auth.credits}</li> ,
+                 <li key="3"><a href="/api/logout">Logout</a></li>
                ];
 
     }
