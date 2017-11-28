e2fe422c74d3e5834adf6862dfb0a6ea76c1a974
Rohan Mehta <1411081+rohan-git@users.noreply.github.com>
2017-11-28 17:14:36 -0500

    api fx to create new survey ;; survey model ;; surveyCredit cheking middleware

--- models/Survey.js
+++ models/Survey.js
@@ -1,13 +1,18 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
+const RecipientSchema = require('./Recpient');
+
 const surveySchema = new Schema({
   title: String,
   body: String,
   subject: String,
-  recipients: [String],  // testing array in model
+  recipients: [RecipientSchema],
   yes: { type: Number, default: 0},
   no: {type: Number, default: 0},
+  _user: { type: Schema.Types.ObjectId, ref: 'User'}, // foreign key relationship
+  dateSent: Date,
+  lastResponded: Date
 });
 
 mongoose.model('survey', surveySchema);
---
+++ routes/surveyRoutes.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const requireLogin = require('../middlewares/requireLogin');
+const requireCredits = require('../middlewares/requireCredits');
+
+const Survey = mongoose.model('Survey');
+
+module.exports = app => {
+
+  app.post('/api/surveys', requireLogin, requireCredits,  (req, res) => {
+
+    const { title, subject, body, recipients } = req.body;
+
+    const survey = new Survey({
+
+      title,
+      subject,
+      body,
+      recipients: recipients.split(',').map(email => ({ email.trim() })),
+      _user: req.user.id,
+      dateSent: Date.now(),
+    });
+
+  });
+
+};
