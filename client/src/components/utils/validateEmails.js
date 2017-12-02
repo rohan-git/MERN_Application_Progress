4f955d12b1d777c2dc88f0aa84989fd98e847a53
rohan <1411081+rohan-git@users.noreply.github.com>
2017-12-02 15:27:15 -0500

    BUGFIX -- changed email string to regex object

--- client/src/components/utils/validateEmails.js
+++ client/src/components/utils/validateEmails.js
@@ -1,9 +1,10 @@
-const EMAIL_REGEX = '/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/';
+const EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
 export default (emails) => {
 
   const invalidEmails = emails.split(',')
-                              .map(email => email.trim().filter(re => re.test(email) === false));
+                              .map(email => email.trim())
+                              .filter(email => EMAIL_REGEX.test(email) === false);
 
    if(invalidEmails.length){
      return `These emails are invalid: ${invalidEmails}`;
